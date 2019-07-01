7a210c85bc19ab6ce0daf5050fab6fa37884311b
Corey Byrum <Corey.Byrum@learnondemandsystems.com>
2019-07-01 10:12:36 -0700

    Other pages - scaffolding

---
+++ js/siteNavigation.js
@@ -0,0 +1,26 @@
+var portfolioUrl = "http://www.coreybyrum7.github.io/pages/portfolio.html";
+var lifeUrl = "http://www.coreybyrum7.github.io/pages/life.html";
+
+$(document).ready(function () {
+
+    $(".life").on('click', function () { 
+        window.location.href = lifeUrl;
+    });
+
+    $(".life").on('keyup', function (e) {
+        if (e.keyCode == 13) {
+            window.location.href = lifeUrl;
+        }
+    });
+
+    $(".portfolio").on('click', function () { 
+        window.location.href = portfolioUrl;
+    });
+
+    $(".portfolio").on('keyup', function (e) {
+        if (e.keyCode == 13) {
+            window.location.href = portfolioUrl;
+        }
+    });
+
+});

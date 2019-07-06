7c0f14d542e2d7a40ff744e3d72ccd181dd3376e
Corey Byrum <Corey.Byrum@learnondemandsystems.com>
2019-07-06 16:53:25 -0700

    Hiding header when opening image

--- js/life.js
+++ js/life.js
@@ -13,11 +13,6 @@ $(document).ready(function () {
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-        modal.style.display = "none";
-    }
-
 });
 
 var imageClick = function (elem) {
@@ -31,6 +26,7 @@ var imageClick = function (elem) {
     var modal = document.getElementById('myModal');
     var modalImg = document.getElementById('modal-image');
     var modalCaption = document.getElementById('modal-caption');
+    $('header.mdl-layout__header').addClass('darkHeader');
     modal.style.display = "block";
     modalImg.src = imageSrc;
     modalCaption.innerHTML = alt;
@@ -38,6 +34,7 @@ var imageClick = function (elem) {
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
+        $('header.mdl-layout__header').removeClass('darkHeader');
         modal.style.display = "none";
     }
 }
